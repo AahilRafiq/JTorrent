d671b6040740a23f38626f1f0726d5ea80114b29
AahilRafiq <aahil.rafiq136@gmail.com>
2025-09-26 11:45:27 +0530

    bencode dictionary

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,11 @@
 package com.example;
 
 import com.example.bencode.BencodeDecoder;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String []args) {
-        System.out.println(BencodeDecoder.decodeString(0, "11:hello world").first);
-        System.out.println(BencodeDecoder.decodeInt(0, "i234e").first);
+        var map = BencodeDecoder.decodeDictionary(0, "d4:infod6:lengthi12345e4:name10:myfile.txt6:pieces40:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX12:piece lengthi16384ee8:announce29:http://tracker.com/announcee\n".getBytes(StandardCharsets.UTF_8)).first;
+        System.out.println(BencodeDecoder.decodeLong(0, "i234e".getBytes(StandardCharsets.UTF_8)).first);
     }
 }
--- src/main/java/com/example/bencode/BencodeDecoder.java
+++ src/main/java/com/example/bencode/BencodeDecoder.java
@@ -2,37 +2,82 @@ package com.example.bencode;
 
 import com.example.helpers.Pair;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BencodeDecoder {
-    public static Pair<Integer,Integer> decodeInt(int index, String input) {
-        StringBuilder sb = new StringBuilder();
+
+    public static Pair<Long,Integer> decodeLong(int index, byte[] input) {
+        List<Byte> bytes = new ArrayList<>();
 
         index++;
-        while (input.charAt(index) != 'e') {
-            sb.append(input.charAt(index));
+        while (index < input.length && input[index] != 'e') {
+            bytes.add(input[index]);
             index++;
         }
         index++;
 
-        return new Pair<>(Integer.parseInt(sb.toString()), index);
+        return new Pair<>(Long.parseLong(toUtf8String(bytes)), index);
     }
 
-    public static Pair<String,Integer> decodeString(int index, String input) {
-        StringBuilder sb = new StringBuilder();
+    public static Pair<String,Integer> decodeString(int index,  byte[] input) {
+        List<Byte> bytes = new ArrayList<>();
 
         int colonIdx = index;
-        while (input.charAt(colonIdx) != ':') {
-            sb.append(input.charAt(colonIdx));
+        while (index < input.length && input[colonIdx] != ':') {
+            bytes.add(input[colonIdx]);
             colonIdx++;
         }
 
-        int length = Integer.parseInt(sb.toString());
+        int length = Integer.parseInt(toUtf8String(bytes));
         index = colonIdx+1;
-        sb.setLength(0);
+        bytes.clear();
+
         while(length-- > 0) {
-            sb.append(input.charAt(index));
+            bytes.add(input[index]);
             index++;
         }
 
-        return new Pair<>(sb.toString(), index);
+        System.out.println(toUtf8String(bytes));
+
+        return new Pair<>(toUtf8String(bytes), index);
+    }
+
+    public static Pair<Map<String,Object>,Integer> decodeDictionary(int index, byte[] input) {
+        Map<String,Object> map = new HashMap<>();
+        index++;
+
+        while(index < input.length && input[index] != 'e') {
+            Pair<String,Integer> pair1 = decodeString(index, input);
+            index = pair1.second;
+
+            if(input[index] == 'i') {
+                Pair<Long, Integer> pair2 = decodeLong(index, input);
+                index = pair2.second;
+                map.put(pair1.first, pair2.first);
+            } else if(input[index] == 'd'){
+                Pair<Map<String, Object>, Integer> pair3 = decodeDictionary(index, input);
+                index = pair3.second;
+                map.put(pair1.first, pair3.first);
+            } else {
+                Pair<String,Integer> pair2 = decodeString(index, input);
+                index = pair2.second;
+                map.put(pair1.first, pair2.first);
+            }
+        }
+
+        index++;
+        return new Pair<>(map, index);
+    }
+
+    private static String toUtf8String(List<Byte> byteList) {
+        byte[] bytes = new byte[byteList.size()];
+        for (int i = 0; i < byteList.size(); i++) {
+            bytes[i] = byteList.get(i);
+        }
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 }
